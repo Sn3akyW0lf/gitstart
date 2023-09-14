05334e6122166f439068444237997a37bd9095a4
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-09-14 10:41:17 +0530

    trying out by classname and by tagname

--- main.js
+++ main.js
@@ -44,12 +44,31 @@ var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 
-items[0].textContent = 'Hello 0';
-items[1].textContent = 'Hello 1';
-items[2].textContent = 'Hello 2';
-items[3].textContent = 'Hello 3';
-items[2].style.backgroundColor = 'green';
+// items[0].textContent = 'Hello 0';
+// items[1].textContent = 'Hello 1';
+// items[2].textContent = 'Hello 2';
+// items[3].textContent = 'Hello 3';
+// items[2].style.backgroundColor = 'green';
 
 for(var i = 0; i < items.length; i++) {
     items[i].style.fontWeight = 'bold';
 }
+
+//Get Element by Tag Name
+
+var li = document.getElementsByTagName('li');
+
+console.log(li);
+console.log(li[1]);
+
+li[0].textContent = 'Hello 0';
+li[1].textContent = 'Hello 1';
+li[2].textContent = 'Hello 2';
+li[3].textContent = 'Hello 3';
+// li[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < li.length; i++) {
+    li[i].style.backgroundColor = '#eee';
+}
+
+
