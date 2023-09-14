3c2ef5d2b8a0ff5bce2f2408836daa517956e225
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-09-14 10:30:14 +0530

    changes in js font and styling

--- main.js
+++ main.js
@@ -2,12 +2,54 @@
 
 // console.dir(document);
 
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.all);
-console.log(document.all[10]);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
 
-document.all[10].textContent = 'Hello';
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+//Get element by ID
+
+// console.log(document.getElementById('header-title'));
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+console.log(headerTitle);
+
+// headerTitle.textContent='Hello';
+// headerTitle.innerText="Goodbye";
+
+// headerTitle.innerHTML = '<h3> Hello </h3>';
+
+// headerTitle.style.borderBottom = 'solid 3px black';
+header.style.borderBottom = 'solid 3px black';
+
+var add_items = document.querySelector('h2');
+
+add_items.style.fontWeight = 'bold';
+add_items.style.color = 'green';
+
+
+//Get Elements by Class Name
+
+var items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+console.log(items[1]);
+
+items[0].textContent = 'Hello 0';
+items[1].textContent = 'Hello 1';
+items[2].textContent = 'Hello 2';
+items[3].textContent = 'Hello 3';
+items[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
